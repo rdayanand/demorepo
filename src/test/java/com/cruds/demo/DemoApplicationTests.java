25dea1af0f0b0594a8660d897bfaa871c026ab8d
rdayanand <anand.daya@gmail.com>
2019-02-09 20:59:32 +0530

    added new Junit

--- src/test/java/com/cruds/demo/DemoApplicationTests.java
+++ src/test/java/com/cruds/demo/DemoApplicationTests.java
@@ -1,17 +1,44 @@
 package com.cruds.demo;
 
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@AutoConfigureMockMvc
 public class DemoApplicationTests {
 
+    @Autowired
+    private MockMvc mvc;
+	
 	@Test
 	public void contextLoads() {
 	}
 
+    @Test
+    public void getHello() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get("/").accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().string(equalTo("Greetings from Spring Boot!")));
+    }	
+
+    @Test
+    public void getHome() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get("/home").accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().string(equalTo("Greetings from Home !")));
+    }    
+    
 }
 
